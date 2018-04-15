c92c474c27f58f69c4def35fcd65e4c8627cc46c
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 19:10:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,13 @@
   def prime?(x)
     if x <= 1
       return false
-  elsif x % 2 == 0
-   return false
  else
    range_max = Math.sqrt(x).to_i 
    range_array = (2..range_max).to_a
    range_array.each do |int|
-     if int % x == 0
+     if x % int == 0
         return false
       end
     end
-    return prime
+  else return true
   end
