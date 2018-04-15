19c6d0404755e4882b35793c3823142db9442d67
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 19:01:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 # Add  code here!
-  def prime?()
-    prime = true
+  def prime?(x)
+    if x <= 1
+      return false
+  elsif x % 2 == 0
+   return false
     for x in 2..Math.sqrt(self).to_i
       if (self % x == 0)
         prime = false
