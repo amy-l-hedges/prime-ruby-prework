e1c27615a7f1de65a123e276fa46a07e0c350dcb
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-04-15 18:57:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+  def prime?()
+    prime = true
+    for x in 2..Math.sqrt(self).to_i
+      if (self % x == 0)
+        prime = false
+        break
+      end
+    end
+    return prime
+  end
